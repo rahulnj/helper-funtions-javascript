dccffcbe62bd1bef6e0b4f04263d10fbf3d2ed8c
rahulnj~ <rahuljayaraman8@gmail.com>
2022-08-09 23:37:46 +0530

    added memoization

---
+++ memoization.js
@@ -0,0 +1,21 @@
+//The technique where we cache the results of a
+// operation and use the cached value to avoid
+//repeating the same operation again and again.
+
+//Example:
+
+function createGetData(data) {
+  let isCached = false
+  let cachedValue = null
+  return function () {
+    if (isCached) {
+      return cachedValue
+    }
+    console.log('fetching data')
+    let result = 123
+    cachedValue = result
+    isCached = true
+    return result
+  }
+}
+console.log(createGetData())
