87a75b9b942023d2e3a11e58231913849bfaa6e6
rahulnj~ <rahuljayaraman8@gmail.com>
2022-08-18 22:24:25 +0530

    added the remaining functions

--- helperFunctions.ts
+++ helperFunctions.ts
@@ -177,4 +177,20 @@ function numberWithCommas(x: number) {
     );
 };
 
+//using set method to remove duplicates from array
+const removeArrayDuplicatesUsingSet = (array: any[]) => {
+  return Array.from(new Set(array));
+}
 
+//using filter method to remove duplicates from array
+const removeArrayDuplicatesUsingFilter = (array: any[]) => {
+  return array.filter((item, index) => array.indexOf(item) === index);
+}
+
+//using reduce method to remove duplicates from array
+const removeArrayDuplicatesUsingReduce = (array:any[]) => {
+  return array.reduce(
+    (partial, item) => (partial.includes(item) ? partial : [...partial, item]),
+    []
+  )
+}
