01555d99d3bae6abbd5c02195c918ee03a8fa982
rahulnj~ <rahuljayaraman8@gmail.com>
2022-08-21 21:49:42 +0530

    removed the test file

--- test.js
+++
@@ -1,4 +0,0 @@
-// const differnceBetweenDates = (date1, date2) =>
-//   Math.ceil(Math.abs(date1.getTime() - date2.getTime()) / (1000 * 60 * 60 * 24))
-
-console.log(differnceBetweenDates(new Date(2012, 1, 1), new Date(2012, 4, 6)))
