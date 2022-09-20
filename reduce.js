d6b8cf76fc2f991ec1da3f84b669f95f0fb771e4
rahulnj~ <rahuljayaraman8@gmail.com>
2022-09-20 21:06:15 +0530

    added reduce file for javascript array method reduce usecases

---
+++ reduce.js
@@ -0,0 +1,13 @@
+//Javascript array method reduce usecases.
+
+const array = [
+  { item: 'item1', price: 10 },
+  { item: 'item1', price: 20 },
+  { item: 'item1', price: 30 },
+]
+
+const reduce1Value = array.reduce((total, item) => {
+  return total + item.price
+}, 0)
+
+console.log({ reduce1Value })
