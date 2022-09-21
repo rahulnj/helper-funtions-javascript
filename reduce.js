8eb8a91b315b062d4bf20c16593f417f3d83c3c8
rahulnj~ <rahuljayaraman8@gmail.com>
2022-09-21 23:16:17 +0530

    reduce-v2 reduce usecases

--- reduce.js
+++ reduce.js
@@ -1,13 +1,29 @@
 //Javascript array method `reduce` usecases.
 
-const array = [
+const array1 = [
   { item: 'item1', price: 10 },
   { item: 'item1', price: 20 },
   { item: 'item1', price: 30 },
 ]
 
-const reduce1Value = array.reduce((total, item) => {
+const reduce1Value = array1.reduce((total, item) => {
   return total + item.price
 }, 0)
 
 console.log({ reduce1Value })
+
+const array2 = [
+  { name: 'Rahul', age: 21 },
+  { name: 'Sooraj', age: 18 },
+  { name: 'Ram', age: 21 },
+  { name: 'kyle', age: 13 },
+]
+
+const reduce2Value = array2.reduce((groupedPeople, person) => {
+  const age = person.age
+  if (groupedPeople[age] == null) groupedPeople[age] = []
+  groupedPeople[age].push(person)
+  return groupedPeople
+}, {})
+
+console.log({ reduce2Value })
